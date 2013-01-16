a393f5fede6b703d7c2e5f48fc59a5c158ba63dd
Mark Koester <markwkoester@gmail.com>
2013-01-16 12:52:26 +0100

    tobar_msg module with content type export for messages, javascript for show/hide and settings page to control general settings

---
+++ topbar_msg.js
@@ -0,0 +1,47 @@
+(function ($) {
+  jQuery.fn.exists = function(){return this.length>0;}	
+  Drupal.behaviors.topBarMsg = {
+    attach: function(context, settings) {
+		$(document).ready(function() {
+	      var text = '<div id="topbarmsg-wrapper">';
+	          text += '<div id="topbarmsg-container">';
+	          text += '<div id="topbarmsg-content">'+ settings.TopBarMsg.message;
+	          text += '<a href="'+ settings.TopBarMsg.link +'" title="'+ settings.TopBarMsg.link_text +'" target="_blank" id="topbarmsg-logo">'+ settings.TopBarMsg.link_text +'</a></div>';
+			  text += '<a href="#close" id="topbarmsg-close">Close</a>';
+			  text += '<div id="topbarmsg-shadow"></div></div>';
+			  text += '</div><a href="#close" id="topbarmsg-open" style="">Open</a>';
+		      $('body').prepend(text);
+	          $('body').css({"padding-top" : "30px"});
+		   if (settings.TopBarMsg.autohide) {
+			  var seconds = !isNaN(settings.TopBarMsg.autohide_seconds) ? settings.TopBarMsg.autohide_seconds*1000 : 5000;
+	          setting_timeout(seconds);
+	       }
+		   $("#topbarmsg-open").click(function() {
+	         open_topbar();
+		   });
+		   $("#topbarmsg-close").click(function() {
+	         close_topbar();
+		   });
+		});
+		function setting_timeout(seconds) {
+		  return setTimeout(function() {
+	       close_topbar();
+	      }, seconds);
+		}
+		function close_topbar() {
+		  if (!$('#toolbar').exists()) {	
+		    $('body').animate({ 'padding-top' : '0' }, 'slow');
+		  }
+	      $("#topbarmsg-wrapper").slideUp();
+	      $("#topbarmsg-open").slideDown();	
+		}
+		function open_topbar() {
+	      if (!$('#toolbar').exists()) {	
+		    $('body').animate({ 'padding-top' : '30' }, 'slow');	
+		  }
+		  $("#topbarmsg-wrapper").slideDown();
+		  $("#topbarmsg-open").slideUp();	
+		}
+    }
+  }
+})(jQuery);
