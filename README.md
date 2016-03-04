1e8e88961c285b35e872d2a50091e2190ab28b70
Patrick Waller <pat@patrickwaller.com>
2016-03-03 20:13:21 -0800

    Major reformatting.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Provides a Drupal-only alternative to HelloBar. It creates a top region area for displaying call-to-action messages and an admin UI for creating those messages with text and links, etc.
+The Topbar Message module allows the administrator to add a configurable 
+Topbar Message to the page. Display can be set either as site wide 
+or controlled via the context module.
+
+Installation
+------------
+Standard module installation applies.
+
+Menus
+-----
+The only menu item is for the settings page.
+
+Settings
+--------
+The settings page is at Administer >> Config >> System >> Topbar Message.
+
+This is where you administer Topbar Message settings.
+
+Permissions
+-----------
+The settings page is controlled by the "administer topbar_msg" permission.
--- js/topbar_msg.js
+++ js/topbar_msg.js
@@ -1,10 +1,10 @@
 (function ($) {
     var duration = 500;
 
-	Drupal.behaviors.topBarMsg = {
+    Drupal.behaviors.topbarMsg = {
         setting_timeout: function(seconds) {
             return setTimeout(function() {
-                Drupal.behaviors.topBarMsg.close_topbar(true);
+                Drupal.behaviors.topbarMsg.close_topbar(true);
             }, seconds);
         },
 
@@ -36,69 +36,69 @@
             }
         },
 
-		attach: function(context, settings) {
+        attach: function(context, settings) {
             var $body = $('body', context);
 
-            $body.once('topbarmsg', function() {
-				var text = '<div id="topbarmsg-wrapper"><div id="topbarmsg-container">',
-                    link = settings.TopBarMsg.link ? '<a href="'+ settings.TopBarMsg.link +'" title="'+ settings.TopBarMsg.link_text +'" target="'+ settings.TopBarMsg.link_target +'" id="topbarmsg-logo">'+ settings.TopBarMsg.link_text +'</a>' : '';
+            $body.once('topbarMsg', function() {
+                var text = '<div id="topbarmsg-wrapper"><div id="topbarmsg-container">',
+                  link = settings.topbarMsg.link ? '<a href="'+ settings.topbarMsg.link +'" title="'+ settings.topbarMsg.link_text +'" target="'+ settings.topbarMsg.link_target +'" id="topbarmsg-logo">'+ settings.topbarMsg.link_text +'</a>' : '';
 
-				text += '<div id="topbarmsg-content">' + settings.TopBarMsg.message + link+ '</div>';
-				text += '<a id="topbarmsg-close">Close</a>';
-				text += '<div id="topbarmsg-shadow"></div>';
-				text += '</div></div>';
+                text += '<div id="topbarmsg-content">' + settings.topbarMsg.message + link + '</div>';
+                text += '<a id="topbarmsg-close">Close</a>';
+                text += '<div id="topbarmsg-shadow"></div>';
+                text += '</div></div>';
                 text += '<a id="topbarmsg-open" style="">Open</a>';
 
                 $body.prepend(text);
 
-				$('#topbarmsg-wrapper', $body).hide();
-                $('#topbarmsg-container', $body).css({'color' : settings.TopBarMsg.color_text});
-				$('#topbarmsg-container, #topbarmsg-open', $body).css({'background-color' : settings.TopBarMsg.color_bg});
-				$('#topbarmsg-container #topbarmsg-logo', $body).css({'color' : settings.TopBarMsg.color_link});
+                $('#topbarmsg-wrapper', $body).hide();
+                $('#topbarmsg-container', $body).css({'color' : settings.topbarMsg.color_text});
+                $('#topbarmsg-container, #topbarmsg-open', $body).css({'background-color' : settings.topbarMsg.color_bg});
+                $('#topbarmsg-container #topbarmsg-logo', $body).css({'color' : settings.topbarMsg.color_link});
 
-				if (settings.TopBarMsg.autohide) {
-					var seconds = !isNaN(settings.TopBarMsg.autohide_seconds) ? settings.TopBarMsg.autohide_seconds * 1000 : 5000;
+                if (settings.topbarMsg.autohide) {
+                    var seconds = !isNaN(settings.topbarMsg.autohide_seconds) ? settings.topbarMsg.autohide_seconds * 1000 : 5000;
 
-                    Drupal.behaviors.topBarMsg.setting_timeout(seconds);
-				}
+                    Drupal.behaviors.topbarMsg.setting_timeout(seconds);
+                }
 
-                if (settings.TopBarMsg.cookie) {
+                if (settings.topbarMsg.cookie) {
                     if($.cookie('topbar') === null) {
                         $.cookie('topbar', 'open', {
-                            expires: parseInt(settings.TopBarMsg.cookie_expire),
+                            expires: parseInt(settings.topbarMsg.cookie_expire),
                             path:'/'
                         });
 
-                        Drupal.behaviors.topBarMsg.open_topbar($body, true);
+                        Drupal.behaviors.topbarMsg.open_topbar($body, true);
                     }
                     else if($.cookie('topbar') == 'open') {
-                        Drupal.behaviors.topBarMsg.open_topbar($body);
+                        Drupal.behaviors.topbarMsg.open_topbar($body);
                     }
                     else if($.cookie('topbar') == 'close') {
-                        Drupal.behaviors.topBarMsg.close_topbar($body);
+                        Drupal.behaviors.topbarMsg.close_topbar($body);
                     }
                 }
 
                 $('#topbarmsg-open').click(function() {
-                    if (settings.TopBarMsg.cookie) {
+                    if (settings.topbarMsg.cookie) {
                         $.cookie('topbar', 'open', {
                             path:'/'
                         });
                     }
 
-                    Drupal.behaviors.topBarMsg.open_topbar($body, true);
+                    Drupal.behaviors.topbarMsg.open_topbar($body, true);
                 });
 
                 $('#topbarmsg-close').click(function() {
-                    if (settings.TopBarMsg.cookie) {
+                    if (settings.topbarMsg.cookie) {
                         $.cookie('topbar', 'close', {
                             path:'/'
                         });
                     }
 
-                    Drupal.behaviors.topBarMsg.close_topbar($body, true);
+                    Drupal.behaviors.topbarMsg.close_topbar($body, true);
                 });
-			});
-		}
-	}
+            });
+        }
+    }
 })(jQuery);
